91974c9dc12d7fb8687ea6c3dd3d151d93e77fec
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-20 17:09:55 -0400

    Create MinionSummoner.sol

---
+++ contracts/minion/MinionSummoner.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.17;
+
+contract MinionSummoner {
+    // presented by OpenESQ || LexDAO LLC ~ Use at own risk! || chat with us: lexdao.chat 
+    Minion private minion;
+    address[] public minions;
+
+    event Summoned(address indexed minion, address moloch);
+
+    function summonMinion(address _moloch) public {
+        minion = new Minion(_moloch);
+        
+        minions.push(address(minion));
+        emit Summoned(address(minion), _moloch);
+    }
+}
