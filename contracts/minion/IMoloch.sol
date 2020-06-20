5111157c4873d5e726b49a01e76df902a3fa2452
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-20 17:07:58 -0400

    Create IMoloch.sol
    add mini moloch interface for minion

---
+++ contracts/minion/IMoloch.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.17;
+
+interface IMoloch {
+    function depositToken() external view returns (address);
+    
+    function getProposalFlags(uint256 proposalId) external view returns (bool[6] memory);
+    
+    function submitProposal(
+        address applicant,
+        uint256 sharesRequested,
+        uint256 lootRequested,
+        uint256 tributeOffered,
+        address tributeToken,
+        uint256 paymentRequested,
+        address paymentToken,
+        string calldata details
+    ) external returns (uint256);
+    
+    function withdrawBalance(address token, uint256 amount) external;
+}
