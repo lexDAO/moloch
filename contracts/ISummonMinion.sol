967e2ccee6fb9877af2d0bcdfb93de65a1807eaa
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-27 16:40:00 -0400

    Update ISummonMinion.sol
    conform to deposit token naming

--- contracts/ISummonMinion.sol
+++ contracts/ISummonMinion.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
 interface ISummonMinion {
-    function setMolochSummoner(address _molochSummoner) external;
-    function summonMinion(address _moloch, address _molochApprovedToken) external;
+    function setMolochSummoner(address molochSummoner) external;
+    function summonMinion(address moloch, address molochDepositToken) external;
 }
