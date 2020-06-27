7f6461de46185a0bc40e78c290f53b2a047d1370
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-27 16:36:56 -0400

    Update MinionSummoner.sol
    add trimmings

--- contracts/minion/MinionSummoner.sol
+++ contracts/minion/MinionSummoner.sol
@@ -4,25 +4,22 @@ import "./Minion.sol";
 
 contract MinionSummoner {
     Minion private minion;
+    address[] public molochs; // tracks summoned molochs for set
     address public molochSummoner;
-    address[] public minions;
-    address[] public molochs;
     bool private molochSummonerSet = false; // tracks lock of MinionSummoner to MolochSummoner 
 
-    event Summoned(address indexed minion, address moloch);
+    event Summoned(address indexed minion, address indexed moloch);
     
     function setMolochSummoner(address _molochSummoner) public {
         require(molochSummonerSet == false, "molochSummoner already set");
         molochSummoner = _molochSummoner;
-        molochSummonerSet = true; // locks MinionSummoner to MolochSummoner
+        molochSummonerSet = true; // locks MinionSummoner to MolochSummoner 
     }
 
-    function summonMinion(address _moloch, address _molochApprovedToken) public {
-        require(msg.sender == molochSummoner, "caller not molochSummoner");
-        minion = new Minion(_moloch, _molochApprovedToken);
+    function summonMinion(address moloch, address _molochDepositToken) public {
+        require(msg.sender == molochSummoner, "not molochSummoner");
+        minion = new Minion(moloch, _molochDepositToken);
         
-        minions.push(address(minion));
-        molochs.push(address(_moloch));
-        emit Summoned(address(minion), _moloch);
+        emit Summoned(address(minion), moloch);
     }
 }
