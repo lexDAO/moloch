7aa8da0bb7e22b377bcc10896fb2ed740a83cec9
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-26 20:35:50 -0400

    Update ISummonMinion.sol
    add MolochSummoner set function to chain contracts

--- contracts/ISummonMinion.sol
+++ contracts/ISummonMinion.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.5.17;
 
 interface ISummonMinion {
+    function setMolochSummoner(address _molochSummoner) external;
     function summonMinion(address _moloch, address _molochApprovedToken) external;
 }
