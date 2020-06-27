e416f188885ecd6e4477e7471d1a69054403a9e7
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-27 19:39:18 -0400

    Update MinionSummoner.sol
    add gas optimizations

--- contracts/minion/MinionSummoner.sol
+++ contracts/minion/MinionSummoner.sol
@@ -6,19 +6,26 @@ contract MinionSummoner {
     Minion private minion;
     address[] public molochs; // tracks summoned molochs for set
     address public molochSummoner;
-    bool private molochSummonerSet = false; // tracks lock of MinionSummoner to MolochSummoner 
-
-    event Summoned(address indexed minion, address indexed moloch);
+    uint256 private _status;
+    uint256 private constant _NOT_SET = 0;
+    uint256 private constant _SET = 1;
     
-    function setMolochSummoner(address _molochSummoner) public {
-        require(molochSummonerSet == false, "molochSummoner already set");
+    event Summoned(address indexed minion, address indexed moloch);
+
+    constructor () public {
+        _status = _NOT_SET;
+    }
+
+    function setMolochSummoner(address _molochSummoner) external {
+        require(_status != _SET, "already set");
         molochSummoner = _molochSummoner;
-        molochSummonerSet = true; // locks MinionSummoner to MolochSummoner 
+        _status = _SET; // locks MinionSummoner to MolochSummoner 
     }
 
     function summonMinion(address moloch, address _molochDepositToken) public {
         require(msg.sender == molochSummoner, "not molochSummoner");
         minion = new Minion(moloch, _molochDepositToken);
+        molochs.push(moloch);
         
         emit Summoned(address(minion), moloch);
     }
