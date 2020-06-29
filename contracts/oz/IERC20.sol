9e5e5f80f1d4ab059c8dc2e784a9aa934f080eef
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-29 15:25:14 -0400

    Update IERC20.sol
    abridge comment

--- contracts/oz/IERC20.sol
+++ contracts/oz/IERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
-interface IERC20 { // brief interface for Moloch DAO token txs
+interface IERC20 { // brief interface for moloch token txs
     function balanceOf(address who) external view returns (uint256);
     
     function transfer(address to, uint256 value) external returns (bool);
