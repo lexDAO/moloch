7903e5216b42e66cdef133692f88854d4c6e16eb
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-05 19:20:32 -0400

    Update ISummonMinion.sol
    comment tweak

--- contracts/ISummonMinion.sol
+++ contracts/ISummonMinion.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
-interface ISummonMinion {
+interface ISummonMinion { // brief interface for minion summonings
     function setMolochSummoner(address molochSummoner) external;
     function summonMinion(address moloch, address molochDepositToken) external;
 }
