91996575d0d89edcd7e5506e5a58cda73d96a8ab
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-25 15:29:55 -0400

    Update IWETH.sol
    space out functions to conform to others

--- contracts/IWETH.sol
+++ contracts/IWETH.sol
@@ -2,5 +2,6 @@ pragma solidity 0.5.17;
 
 interface IWETH { // brief interface for ether wrapping contract 
     function deposit() payable external;
+    
     function transfer(address dst, uint wad) external returns (bool);
 }
