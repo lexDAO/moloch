606d7a00a7a31e47403498969b5d28808715de70
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-25 14:27:29 -0400

    Update MolochSummoner.sol
    simplify summoner

--- contracts/MolochSummoner.sol
+++ contracts/MolochSummoner.sol
@@ -5,14 +5,8 @@ import "./ISummonMinion.sol";
 
 contract MolochSummoner {
     Moloch private baal;
-    ISummonMinion public minionSummoner;
-    
+
     event SummonMoloch(address indexed moloch, address[] indexed summoners, address indexed depositToken, uint256 periodDuration, uint256 votingPeriodLength, uint256 gracePeriodLength, uint256 proposalDeposit, uint256 dilutionBound, uint256 processingReward, uint256[] summonerStake, uint256 summoningDeposit, uint256 summoningRate, uint256 summoningTermination, uint256 summoningTime);
-    
-    constructor(address _minionSummoner) public { 
-        minionSummoner = ISummonMinion(_minionSummoner);
-        minionSummoner.setMolochSummoner(address(this)); // lock minionSummoner to molochSummoner
-    }
 
     function summonMoloch(
         address[] memory _summoners,
@@ -44,10 +38,8 @@ contract MolochSummoner {
         
         address moloch = address(baal);
         
-        require(IERC20(_depositToken).transferFrom(msg.sender, moloch, _summoningDeposit), "transfer failed"); // transfer summoning deposit to new moloch
-        
-        minionSummoner.summonMinion(moloch, _depositToken); // summon minion for new moloch
-        
+        IERC20(_depositToken).transferFrom(msg.sender, moloch, _summoningDeposit); // transfer summoning deposit to new moloch
+
         emit SummonMoloch(moloch, _summoners, _depositToken, _periodDuration, _votingPeriodLength, _gracePeriodLength, _proposalDeposit, _dilutionBound, _processingReward, _summonerShares, _summoningDeposit, _summoningRate, _summoningTermination, now);
     }
 }
