1aa04374bf139ebcee08d2848b01ee8713fbfc86
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-26 20:04:37 -0400

    Create ISummonMinion.sol
    recreate minion summoning

---
+++ contracts/ISummonMinion.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.5.17;
+
+interface ISummonMinion { // brief interface for moloch minion summoning
+    function setMolochSummoner(address molochSummoner) external;
+    
+    function summonMinion(address moloch, address molochDepositToken) external;
+}
