4cbfd03b884c33b3d5b23ca744038ad3098a6fab
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-27 10:33:21 -0400

    Delete ISummonMinion.sol

--- contracts/ISummonMinion.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.17;
-
-interface ISummonMinion { // brief interface for moloch minion summoning
-    function setMolochSummoner(address molochSummoner) external;
-    
-    function summonMinion(address moloch, address molochDepositToken) external;
-}
