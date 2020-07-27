3274204776517d7b7385ca9f07e62b309cad48c6
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-26 20:05:31 -0400

    Update MinionSummoner.sol
    add minion summoning lock

--- contracts/minion/MinionSummoner.sol
+++ contracts/minion/MinionSummoner.sol
@@ -4,12 +4,27 @@ import "./Minion.sol";
 
 contract MinionSummoner {
     Minion private minion;
+    address public molochSummoner;
+    uint8 private status;
+    uint8 private NOT_SET;
+    uint8 private constant SET = 1;
     
-    event SummonMinion(address indexed minion, address indexed moloch);
+    event SummonMinion(address indexed minion);
+
+    constructor() public {
+        status = NOT_SET;
+    }
+
+    function setMolochSummoner(address _molochSummoner) external {
+        require(status != SET, "already set");
+        molochSummoner = _molochSummoner;
+        status = SET; // lock minionSummoner to molochSummoner
+    }
 
     function summonMinion(address _moloch, address _molochDepositToken) external {
+        require(msg.sender == molochSummoner, "not molochSummoner");
         minion = new Minion(_moloch, _molochDepositToken); // summon minion for parent moloch
         
-        emit SummonMinion(address(minion), _moloch);
+        emit SummonMinion(address(minion));
     }
 }
