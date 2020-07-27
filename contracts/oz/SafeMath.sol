5a212b56b31e0baf89801005c4bc8653ea1e48e2
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-27 10:34:58 -0400

    Update SafeMath.sol
    simplify to core moloch functions

--- contracts/oz/SafeMath.sol
+++ contracts/oz/SafeMath.sol
@@ -14,22 +14,4 @@ library SafeMath { // wrappers over solidity arithmetic operations with added ov
 
         return c;
     }
-    
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b);
-
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0);
-        uint256 c = a / b;
-
-        return c;
-    }
 }
