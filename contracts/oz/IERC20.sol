40afcf474731e06bbb9ec030e91de79aefadb9b7
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-08-14 18:36:56 -0400

    Update IERC20.sol
    conform to oz

--- contracts/oz/IERC20.sol
+++ contracts/oz/IERC20.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.5.17;
 
-interface IERC20 { // brief interface for moloch erc20 token txs
-    function balanceOf(address who) external view returns (uint256);
+interface IERC20 { // brief interface for erc20 token txs
+    function balanceOf(address account) external view returns (uint256);
     
-    function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
