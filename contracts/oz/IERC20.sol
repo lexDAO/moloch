e65151867280baa01451cb54df06ed40e85c30e5
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-08-22 20:25:25 -0400

    Update IERC20.sol
    remove comment

--- contracts/oz/IERC20.sol
+++ contracts/oz/IERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
-interface IERC20 { // brief interface for erc20 token txs
+interface IERC20 { 
     function balanceOf(address account) external view returns (uint256);
     
     function transfer(address recipient, uint256 amount) external returns (bool);
