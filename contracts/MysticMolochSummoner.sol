7703a954605e0c0e255b3ebb9faf6b58009aad5d
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-08-22 20:22:17 -0400

    Update MysticMolochSummoner.sol
    eip1167

--- contracts/MysticMolochSummoner.sol
+++ contracts/MysticMolochSummoner.sol
@@ -1,9 +1,14 @@
 pragma solidity 0.5.17;
 
 import "./Moloch.sol";
+import "./CloneFactory.sol";
 
-contract MysticMolochSummoner { 
-    MysticMoloch private baal;
+contract MysticMolochSummoner is CloneFactory { 
+    address payable public template;
+    
+    constructor (address payable _template) public {
+        template = _template;
+    }
 
     event SummonMoloch(address indexed baal, address depositToken, address stakeToken, address[] summoner, uint256[] summonerShares, uint256 summoningDeposit, uint256 proposalDeposit, uint256 processingReward, uint256 periodDuration, uint256 votingPeriodLength, uint256 gracePeriodLength, uint256 dilutionBound, uint256 summoningTime);
  
@@ -19,8 +24,10 @@ contract MysticMolochSummoner {
         uint256 _votingPeriodLength,
         uint256 _gracePeriodLength,
         uint256 _dilutionBound
-    ) public {
-        baal = new MysticMoloch(
+    ) public returns (address) {
+        MysticMoloch baal = MysticMoloch(createClone(template));
+        
+        baal.init(
             _depositToken,
             _stakeToken,
             _summoner,
@@ -31,10 +38,13 @@ contract MysticMolochSummoner {
             _periodDuration,
             _votingPeriodLength,
             _gracePeriodLength,
-            _dilutionBound);
+            _dilutionBound
+        );
         
         require(IERC20(_depositToken).transferFrom(msg.sender, address(baal), _summonerDeposit), "!transfer"); // transfer summoner deposit to new moloch
-
+        
         emit SummonMoloch(address(baal), _depositToken, _stakeToken, _summoner, _summonerShares, _summonerDeposit, _proposalDeposit, _processingReward, _periodDuration, _votingPeriodLength, _gracePeriodLength, _dilutionBound, now);
+        
+        return address(baal);
     }
 }
