5bbe3f698aca98ec7cfcf2c2edc5777a16bc7536
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-08-25 15:58:25 -0400

    Update IWETH.sol

--- contracts/IWETH.sol
+++ contracts/IWETH.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
-interface IWETH { // brief interface for canonical ether token wrapper contract 
+interface IWETH { // brief interface for canonical ether token wrapper 
     function deposit() payable external;
     
     function transfer(address dst, uint wad) external returns (bool);
