8025a3e1d224c8079d060960b8639d340f1e6158
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-21 13:43:56 -0400

    Delete IWETH.sol
    remove unnecessary wETH interface (ierc20 works here)

--- contracts/IWETH.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.6.12;
-
-interface IWETH { // brief interface for canonical ether token wrapper 
-    function deposit() external payable;
-    
-    function transfer(address dst, uint wad) external returns (bool);
-}
