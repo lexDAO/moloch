dc03bd1cb84fa921855df9fab82f0f5ea184655b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-21 13:45:39 -0400

    Update SafeMath.sol

--- contracts/oz/SafeMath.sol
+++ contracts/oz/SafeMath.sol
@@ -25,7 +25,7 @@ library SafeMath { // arithmetic wrapper for unit under/overflow check
 
         return c;
     }
-
+    
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b > 0);
         uint256 c = a / b;
