28b796bfb866595cabf62b8211a226a0f0c9ab33
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-21 13:45:16 -0400

    Update SafeERC20.sol
    spacing

--- contracts/oz/SafeERC20.sol
+++ contracts/oz/SafeERC20.sol
@@ -2,18 +2,17 @@ pragma solidity 0.6.12;
 
 library SafeERC20 { // wrapper around erc20 token tx for non-standard contract - see openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol
     using Address for address;
-
+    
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
         _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
-
+    
     function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
         _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
-
-   function _callOptionalReturn(IERC20 token, bytes memory data) private {
+    
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
         require(address(token).isContract(), "SafeERC20: call to non-contract");
-
         (bool success, bytes memory returnData) = address(token).call(data);
         require(success, "SafeERC20: low-level call failed");
 
