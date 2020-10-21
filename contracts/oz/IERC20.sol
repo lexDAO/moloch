7a3a11b209c2be1b75d6adf3cf4b93fce9fed2b5
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-21 13:44:20 -0400

    Update IERC20.sol
    spacing

--- contracts/oz/IERC20.sol
+++ contracts/oz/IERC20.sol
@@ -2,8 +2,6 @@ pragma solidity 0.6.12;
 
 interface IERC20 { // brief interface for erc20 token tx
     function balanceOf(address account) external view returns (uint256);
-    
     function transfer(address recipient, uint256 amount) external returns (bool);
-
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
